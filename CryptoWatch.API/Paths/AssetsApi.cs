0e868000d44f07f99e329bba09eb325d9955258a
Rafael Lúcio <rafaellucio@pm.me>
2023-07-16 21:23:38 -0300

    feat: missing Assets.List overloads

--- CryptoWatch.API/Paths/AssetsApi.cs
+++ CryptoWatch.API/Paths/AssetsApi.cs
@@ -7,9 +7,19 @@ public readonly struct AssetsApi
 
     internal AssetsApi(ApiConfiguration apiConfiguration) => _apiConfiguration = apiConfiguration;
 
-    public Task<HttpResponseMessage> List(uint pagination = 5_000) => _apiConfiguration.CreateClient()
-        .GetAsync($"{Route}?limit={pagination}");
+    public Task<HttpResponseMessage> List() =>
+        _apiConfiguration.CreateClient()
+            .GetAsync($"{Route}");
 
-    public Task<HttpResponseMessage> Details(string asset, uint pagination = 20_000) => _apiConfiguration.CreateClient()
-        .GetAsync($"{Route}/{asset}?limit={pagination}");
+    public Task<HttpResponseMessage> List(uint limit) =>
+        _apiConfiguration.CreateClient()
+            .GetAsync($"{Route}?limit={limit}");
+
+    public Task<HttpResponseMessage> Details(string asset) =>
+        _apiConfiguration.CreateClient()
+            .GetAsync($"{Route}/{asset}");
+
+    public Task<HttpResponseMessage> Details(string asset, uint limit) =>
+        _apiConfiguration.CreateClient()
+            .GetAsync($"{Route}/{asset}?limit={limit}");
 }
