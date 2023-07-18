27d14d6f4825dfb106f9133f25c3faf8025cdc95
Rafael Lúcio <rafaellucio@pm.me>
2023-07-17 23:33:59 -0300

    feat: markets and assets default listings

--- CryptoWatch.API.Tests.Integration/MarketsTests.cs
+++ CryptoWatch.API.Tests.Integration/MarketsTests.cs
@@ -1,3 +1,5 @@
+using CryptoWatch.API.Types;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -35,8 +37,43 @@ public class MarketsTests : IAsyncLifetime
     [Fact]
     public async Task Asserts_CryptoWatchApiMarketsListing_JsonResponseDeserialization()
     {
-        var list = await new CryptoWatchApi(_httpClientFactory.Object).Markets.ListAsyncTask();
+        var marketListing = await new CryptoWatchApi(_httpClientFactory.Object).Markets.ListAsyncTask();
 
-        Console.WriteLine();
+        marketListing.Should()
+            .BeOfType<MarketCollection>();
+        marketListing.Result.First()
+            .Should()
+            .BeOfType<MarketCollection.Collection>();
+        marketListing.Result.Should()
+            .HaveCount(20_000);
+        marketListing.Result.First()
+            .Active.Should()
+            .BeTrue();
+        marketListing.Result.First()
+            .Exchange.Should()
+            .Be("bitfinex");
+        marketListing.Result.First()
+            .Id.Should()
+            .Be(1);
+        marketListing.Result.First()
+            .Pair.Should()
+            .Be("btcusd");
+        marketListing.Result.First()
+            .Route.Should()
+            .Be("https://api.cryptowat.ch/markets/bitfinex/btcusd");
+        marketListing.Cursor.Should()
+            .BeOfType<Cursor>();
+        marketListing.Cursor.HasMore.Should()
+            .BeTrue();
+        marketListing.Cursor.Last.Should()
+            .Be("TF8j1fnzBNxi7bQkOQgcFb2r9X_jzp0jq8PmiYcAnGzjlUHY93Sg7AdMzlzpvQ");
+        marketListing.Allowance.Should()
+            .BeOfType<Allowance>();
+        marketListing.Allowance.Cost.Should()
+            .Be(0.003M);
+        marketListing.Allowance.Remaining.Should()
+            .Be(9.997M);
+        marketListing.Allowance.Upgrade.Should()
+            .Be("For unlimited API access, create an account at https://cryptowat.ch");
     }
 }
