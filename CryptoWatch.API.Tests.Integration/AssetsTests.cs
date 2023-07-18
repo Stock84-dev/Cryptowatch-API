ee0ec7236fee42ed9cf6f73616c346740987ecef
Rafael Lúcio <rafaellucio@pm.me>
2023-07-17 23:33:59 -0300

    feat: markets and assets default listings

--- CryptoWatch.API.Tests.Integration/AssetsTests.cs
+++ CryptoWatch.API.Tests.Integration/AssetsTests.cs
@@ -1,3 +1,5 @@
+using CryptoWatch.API.Types;
+using FluentAssertions;
 using Moq;
 using Xunit;
 
@@ -33,10 +35,45 @@ public class AssetsTests : IAsyncLifetime
     }
 
     [Fact]
-    public async Task Asserts_CryptoWatchApiAssetsListing_HttpResponse()
+    public async Task Asserts_CryptoWatchApiAssetsListing_JsonResponseDeserialization()
     {
-        var list = await new CryptoWatchApi(_httpClientFactory.Object).Assets.ListAsyncTask();
+        var assetListing = await new CryptoWatchApi(_httpClientFactory.Object).Assets.ListAsyncTask();
 
-        Console.WriteLine();
+        assetListing.Should()
+            .BeOfType<AssetCollection>();
+        assetListing.Result.First()
+            .Should()
+            .BeOfType<AssetCollection.Collection>();
+        assetListing.Result.Should()
+            .HaveCount(4842);
+        assetListing.Result.First()
+            .Fiat.Should()
+            .BeFalse();
+        assetListing.Result.First()
+            .Route.Should()
+            .Be("https://api.cryptowat.ch/assets/00");
+        assetListing.Result.First()
+            .Id.Should()
+            .Be(182298);
+        assetListing.Result.First()
+            .Name.Should()
+            .Be("zer0zer0");
+        assetListing.Result.First()
+            .Symbol.Should()
+            .Be("00");
+        assetListing.Cursor.Should()
+            .BeOfType<Cursor>();
+        assetListing.Cursor.HasMore.Should()
+            .BeFalse();
+        assetListing.Cursor.Last.Should()
+            .Be("zVeAqhIX15waAlZ__raauv4gLhC-vC_RvnmahCyud7wV0r8yxV5ra9BJgOGmHg");
+        assetListing.Allowance.Should()
+            .BeOfType<Allowance>();
+        assetListing.Allowance.Cost.Should()
+            .Be(0.002M);
+        assetListing.Allowance.Remaining.Should()
+            .Be(9.98M);
+        assetListing.Allowance.Upgrade.Should()
+            .Be("For unlimited API access, create an account at https://cryptowat.ch");
     }
 }
